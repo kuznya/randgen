cfe6adffb22fd2e73b2dba0db62d8457c2630bcf
kuznya <kuznya1@gmail.com>
2017-08-07 02:34:17 +0300

    v0.01

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# randgen
+## randgen
---
+++ js/lib.js
@@ -0,0 +1,28 @@
+function print(s) { document.write(s+'<br>\n'); }
+function rand(limit) { return (Math.random()*limit)|0; }
+
+function choice(abc) {
+    const idx = rand(abc.length);
+    return abc[idx];
+}
+
+const abc_36        = '0123456789abcdefghijklmnopqrstuvwxyz';
+const abc_base64    = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
+const abc_nato      = [
+    'alpha',    'bravo',    'charlie',  'delta',
+    'echo',     'foxtrot',  'golf',     'hotel',
+    'india',    'juliet',   'kilo',     'lima',
+    'mike',     'november', 'oscar',    'papa',
+    'quebec',   'romeo',    'sierra',   'tango',
+    'uniform',  'victor',   'whiskey',  'x-ray',
+    'yankee',   'zulu'
+];
+
+function randstr(abc, cnt) {
+    let a = [];
+    for (let i=0; i<cnt; i++) {
+        const s = choice(abc);
+        a.push(s)
+    }
+    return a.join('');
+}
