c20892bbc41b47acd3d9c3dc9f4e842c474c5358
kuznya <kuznya1@gmail.com>
2020-11-11 01:10:45 +0200

    maze02-03 added

--- maze02/js/CBoard2.js
+++ maze02/js/CBoard2.js
@@ -1,10 +1,15 @@
-function rand(limit) { return (Math.random()*limit)|0; }
+const rand = (limit) => (Math.random()*limit)|0
 
-function choice(abc) {
+const choice = (abc) => {
     const idx = rand(abc.length);
     return abc[idx];
 }
 
+const countBits = (n) => {
+  let cnt = 0;
+  for (; n; n >>=1) cnt += n&1
+  return cnt
+}
 
 function CBoard(cfg) {
   this.init(cfg)
@@ -41,7 +46,7 @@ CBoard.prototype.buildDoors = function() {
     a = cube_filled(NY, NX, 4, 0)
 
   // * generate doors
-  console.log('buildDoors: gen')
+  //console.log('buildDoors: gen')
   for (let j = 0; j < NY; j++)
     for (let i = 0; i < NX; i++) {
       a[j][i][0] = !i ? 0 : a[j][i-1][2]
@@ -50,7 +55,7 @@ CBoard.prototype.buildDoors = function() {
       a[j][i][3] = j === NY - 1 ? 0 : rand_door()
     }
   this.mapInfo = a;
-  console.log('buildDoors: mapInfo', a)
+  //console.log('buildDoors: mapInfo', a)
   // * place doors
   for (let j = 0; j < NY; j++)
     for (let i = 0; i < NX; i++) {
@@ -64,6 +69,72 @@ CBoard.prototype.buildDoors = function() {
       for (let bit = 0; bit < 3; bit++, acc >>= 1) {
         aa[(j+1)*4][i*4+1+bit] = acc & 1;
       }
+    }
+}
+
+CBoard.prototype.formRoom = function (j, i, info) {
+  const getOffset = (n) => !n ? -1 : n >> 1;
+  const
+    a = this.board,
+    y = j*4 + 1,
+    x = i*4 + 1
+
+  const fillRoom = (y, x) => {
+    for (let dy = 0; dy < 4; dy++)
+      for (let dx = 0; dx < 4; dx++) {
+        if (dy ===3 && dx === 3) continue;
+        a[y+dy][x+dx] = 0
+      }
+  }
+  const digRoom = (y, x) => {
+    for (let dy = 0; dy < 3; dy++)
+      for (let dx = 0; dx < 3; dx++) {
+        a[y+dy][x+dx] = 1
+      }
+  }
+
+  const digPassage = (y, x, info) => {
+    // openSides > 1
+    const
+      a = this.board,
+      horizontal = getOffset(info[0] | info[2]),
+      vertical = getOffset(info[1] | info[3])
+
+    if (horizontal > -1) {
+      const [d0, d1] = [info[0]? 0 : vertical, info[2]? 2 : vertical]
+      for (let d = d0; d <= d1; d++)
+        a[y + horizontal][x + d] = 1
+    }
+    if (vertical > -1) {
+      const [d0, d1] = [info[1]? 0 : horizontal, info[3]? 2 : horizontal]
+      for (let d = d0; d <= d1; d++)
+        a[y + d][x + vertical] = 1
+    }
+  }
+
+  const openSides = info.reduce((a, v) => a + !!v, 0)
+  const toDigRoom = openSides === 1
+    || countBits(info[0] | info[2]) > 1
+    || countBits(info[1] | info[3]) > 1
+
+  if (toDigRoom) {
+    digRoom(y, x)
+    return
+  }
+  // else
+  digPassage(y, x, info)
+}
+
+CBoard.prototype.buildRooms = function () {
+  const
+    cfg = this.cfg,
+    mapInfo = this.mapInfo,
+    NX = (cfg.resX-1) / 4,
+    NY = (cfg.resY-1) / 4
+
+  for (let j = 0; j < NY; j++)
+    for (let i = 0; i < NX; i++) {
+      this.formRoom(j, i, mapInfo[j][i])
     }
 }
 
